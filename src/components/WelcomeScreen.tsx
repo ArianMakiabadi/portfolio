725f14ed403a26122fcb12f25c498e0193982513
arian <arian.maki@yahoo.com>
2026-06-24 12:42:31 +0200

    ui: welcome screen added

---
+++ src/components/WelcomeScreen.tsx
@@ -0,0 +1,21 @@
+function WelcomeScreen() {
+  return (
+    <div className="welcome-bg-gradient min-h-screen relative">
+      {/* Top bar */}
+      <div className="absolute top-0 h-32 w-full bg-welcome-header">
+        <div className="absolute bottom-0 h-0.5 w-full welcome-top-line" />
+      </div>
+      {/* Content */}
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="translate-x-36 -translate-y-8 font-trebuchet text-6xl font-semibold text-white [text-shadow:1px_1px_3px_rgba(0,0,0,0.4)]">
+          welcome
+        </h1>
+      </div>
+      {/* Bottom bar */}
+      <div className="absolute bottom-0 h-48 w-full bg-welcome-header">
+        <div className="welcome-gold-line h-0.5 w-full" />
+      </div>
+    </div>
+  );
+}
+export default WelcomeScreen;
