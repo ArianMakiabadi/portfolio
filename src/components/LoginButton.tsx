8c96704d8c6a2412028da037f9973b1e877b0013
arian <arian.maki@yahoo.com>
2026-06-25 13:53:21 +0200

    ui: login button implemented

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,31 @@
+function LoginButton() {
+  return (
+    <div className="group relative overflow-hidden rounded-md cursor-pointer">
+      {/* Hover gradient */}
+      <div className="absolute inset-0 bg-linear-to-r from-[#113fa6] via-[#113fa6] to-[#587cdb] opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
+
+      {/* Content */}
+      <div className="relative py-3 px-4.5">
+        <div className="flex gap-4">
+          <div className="box-border h-16 w-16">
+            <img
+              src="/profile-photo.webp"
+              alt="user"
+              className="rounded-md border-[3px] border-white transition-colors duration-300 group-hover:border-amber-400"
+            />
+          </div>
+
+          <div className="font-arial">
+            <p className="text-2xl text-white">Arian Makiabadi</p>
+
+            <p className="text-sm font-bold text-[#000080] transition-colors duration-300 group-hover:text-amber-400">
+              Full-Stack Developer
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginButton;
