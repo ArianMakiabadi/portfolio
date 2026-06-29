0c1de838249b3492b394482700c4ee7846dcde5f
arian <arian.maki@yahoo.com>
2026-06-30 00:14:13 +0200

    feat: add taskbar clock

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -3,6 +3,13 @@ import taskbarBg from "../assets/taskbar/taskbar-bg.webp";
 import systemTray from "../assets/taskbar/system-tray.webp";
 
 function Taskbar() {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
   return (
     <div
       className="relative bg-repeat-x flex items-between"
@@ -16,6 +23,9 @@ function Taskbar() {
       <div className="flex-1" />
 
       <img src={systemTray} alt="system-tray" className="h-full" />
+      <span className="absolute right-3 top-1/2 -translate-y-1/2 text-[11px] text-white font-tahoma">
+        {time}
+      </span>
     </div>
   );
 }
