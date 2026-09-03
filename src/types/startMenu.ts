919d09020e196cae4cc31b8da3aa74e58e10466b
arian <arian.maki@yahoo.com>
2026-09-03 15:42:11 +0200

    types: add start menu types

---
+++ src/types/startMenu.ts
@@ -0,0 +1,13 @@
+export type StartMenuAppItem = {
+  id: string;
+  title: string;
+  subtitle: string;
+  icon: string;
+};
+
+export type StartMenuLinkItem = {
+  id: string;
+  label: string;
+  icon: string;
+  href: string;
+};
